d75829e300760b028f1aed646130cc1a43f0c6d9
cheng <778281938@qq.com>
2021-04-02 17:44:23 +0800

    移动端适配

--- static/front/static/js/chunk-2d0e5b70.63994ec0.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5b70"],{9652:function(e,n,t){"use strict";t.r(n);var i=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("span",[e._v("202101250249520wjkjwziqi60iub7nyz496iwbu2ep03hm247qx379ixl3vtfaj")])])}],c=t("2877"),l={},r=Object(c["a"])(l,i,u,!1,null,null,null);n["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0e5b70.63994ec0.js.map
